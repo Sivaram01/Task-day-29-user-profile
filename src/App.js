c7740c2f9078b43d3d6ab9b97d75ef1478ca2235
Sivaram01 <sivaram.v10@gmail.com>
2021-12-23 19:30:22 +0530

    user-dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter as Router , Route , Switch} from "react-router-dom";
+import Home from './components/home.js' 
+import About from './components/about.js'
+import Navbar from './components/navbar.js'
+import AddUser from './components/addUser';
+import EditUser from './components/editUser.js';
+import User from './components/user.js';
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <div className="app">
+       <Navbar/>
+       <Switch>
+         <Route exact path="/">
+             <Home/>
+           </Route>
+         <Route exact path="/about">
+             <About/>
+           </Route>
+         <Route exact path="/users/add">
+             <AddUser/>
+           </Route>
+         <Route exact path="/users/edit/:id">
+             <EditUser/>
+           </Route>
+         <Route exact path="/users/:id">
+             <User/>
+           </Route>
+         </Switch>
+     </div>
+    </Router>
   );
 }
 
-export default App;
+
+
+
+
+
+
